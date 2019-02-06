64c23477d4dd16fd203e794c79259071bae51b60
Naser Alkhateri <nalkhate@ix-dev.cs.uoregon.edu>
2019-02-05 20:43:39 -0800

    fixed seg faultgit add .!

--- assignment4/reversi.cpp
+++ assignment4/reversi.cpp
@@ -15,7 +15,7 @@ Board::Board(int size){
 	for (i = 0; i < size; i++){
 		m_brd[i] = new string [size];
 		for(j = 0; j < size; j++){
-			m_brd[i][j] = ". ";
+			m_brd[i][j] = ".";
 		}
 	}
 }
@@ -26,10 +26,10 @@ void Board::defaultBoard(){
 
 	int indexX = (m_size/2) - 1;
 	
-	m_brd[indexX][indexX] = "b ";
-	m_brd[indexX+1][indexX+1] = "b ";
-	m_brd[indexX][indexX+1] = "w ";
-	m_brd[indexX+1][indexX] = "w ";
+	m_brd[indexX][indexX] = "b";
+	m_brd[indexX+1][indexX+1] = "b";
+	m_brd[indexX][indexX+1] = "w";
+	m_brd[indexX+1][indexX] = "w";
 	
 	
 	
@@ -39,7 +39,6 @@ void Board::printBoard(){
 	int i,j;
 	
 	
-	int size = m_size;
 	cout << '\t';
 
 	for(i = 0; i < m_size; i++){
@@ -52,34 +51,231 @@ void Board::printBoard(){
     
     cout << i << "\t";
     for(j = 0; j < m_size; j++){
-      cout << m_brd[i][j];
+      cout << m_brd[i][j] << " ";
     }
     cout << endl;
   }	
 }
+void Board::makeMovement(){
+
+  int playerTurn = 1;
+  int x, y;
+  bool isOver = false, isValid = false;
+  while(!isOver){
+    printBoard();
+    cout << "Player " << playerTurn << " turn, input 2 numbers (0-"<< m_size << ") row col: ";
+    cin >> x >> y;
+    while(x < 0 || x > m_size - 1 || y < 0 || y > m_size - 1){
+      cout << "Player " << playerTurn << " turn, input 2 numbers (0-"<< m_size << ") row col: ";
+      cin >> x >> y;
+    }
+    isValid = validMovement(x, y, playerTurn);
+    if (isValid){
+      if (playerTurn == 1){
+	m_brd[x][y] = "b";
+	playerTurn = 2;
+      }else{m_brd[x][y] = "w"; playerTurn = 1;}
+      
+    }else{cout << "invalid choice!" << endl;}
+  }
+}
+
+bool Board::validMovement(int x, int y, int playerNum){
+
+  int varX, varY, i, j;
+  string player;
+  if (playerNum == 1){
+    player = "b";
+  }else{player = "w";}
+  
+  if (y > 0 && player != m_brd[x][y-1] && m_brd[x][y-1] != "."){
+    //left
+    cout << "left" << endl;
+    varY = m_size - y;
+
+	cout << "entered\n";
+	for(i=2;i<=y;i++){
+      if(player == m_brd[x][y-i]){
+		for(j=1;j<m_size;j++){
+	  
+			if(m_brd[x][y-j] == "."){
+				return true;
+			}
+			m_brd[x][y-j] = player;
+		}
+	
+		return true;
+      }
+    }
+  
+  }
+  if(y < m_size-1 && player != m_brd[x][y+1] && m_brd[x][y+1] != "."){
+    //right
+    cout << "right" << endl;
+    varY = m_size - y;
+    if (x == (m_size - 1)){}
+    for(i=2;i<varY;i++){
+      if(player == m_brd[x][y+i]){
+	for(j=1;j<m_size;j++){
+	  
+	  if(m_brd[x][y+j] == "."){
+	    return true;
+	  }
+	  m_brd[x][y+j] = player;
+	}
+	
+	return true;
+      }
+    }
+  }if(x < m_size-1 && player != m_brd[x+1][y] && m_brd[x+1][y] != "."){
+    //down
+    cout << "down" << endl;
+    varX = m_size - x;
+    if (y == (m_size - 1)){cout << endl;
+    }else{
+      for(i=2;i<varX;i++){
+	if(player == m_brd[x+i][y]){
+	  for(j=1;j<m_size;j++){
+	    
+	    if(m_brd[x+j][y] == "."){
+	      return true;
+	    }
+	    m_brd[x+j][y] = player;
+	  }
+	  return true;
+	}
+      }
+    }
+  }
+  if(x > 0 && player != m_brd[x-1][y] && m_brd[x-1][y] != "."){
+    //up
+	cout << "up" << endl;
+    if (y == 0){cout << endl;
+    }else{
+      varX = m_size - x;
+      for(i=2;i<varX;i++){
+	if(player == m_brd[x-i][y]){
+	  for(j=1;j<m_size;j++){
+	    
+	    if(m_brd[x-j][y] == "."){
+	      return true;
+	    }
+	    m_brd[x-j][y] = player;
+	  }
+	  return true;
+	}
+      }
+    }
+  }
+  if(y > 0 && x < m_size-1 && player != m_brd[x+1][y-1] && m_brd[x+1][y-1] != "."){
+    //down-left
+    cout << "down-left" << endl;
+    if (y == (m_size - 1) || x == 0 ){cout << endl;
+    }else{
+      varX = m_size - x;
+      for(i=2;i<varX;i++){
+	if(player == m_brd[x+i][y-i]){
+	  for(j=1;j<m_size;j++){
+	    
+	    if(m_brd[x+j][y-j] == "."){
+	      return true;
+	    }
+	    m_brd[x+j][y-j] = player;
+	  }
+	  
+	  return true;
+	}
+      }
+    }
+  }if(y > 0 && x > 0 && player != m_brd[x-1][y-1] && m_brd[x-1][y-1] != "."){
+    //up-left
+    cout << "up-left" << endl;
+    varX = m_size - x;
+    if (x == 0 || y == 0){cout << endl;
+    }else{
+      for(i=2;i<varX;i++){
+	if(player == m_brd[x-i][y-i]){
+	  for(j=1;j<m_size;j++){
+	    
+	    if(m_brd[x-j][y-j] == "."){
+	      return true;
+	    }
+	    m_brd[x-j][y-j] = player;
+	  }
+	  
+	  return true;
+	}
+      }
+    }
+  }if(y < m_size-1 && x < m_size-1 && player != m_brd[x+1][y+1] && m_brd[x+1][y+1] != "."){
+	cout << "down-right" << endl;
+    varX = m_size - x;
+    if (x == (m_size - 1) || y == (m_size - 1)){cout << endl;
+    }else{
+      for(i=2;i<varX;i++){
+	if(player == m_brd[x+i][y+i]){
+	  for(j=1;j<m_size;j++){
+				  
+	    if(m_brd[x+j][y+j] == "."){
+	      return true;
+	    }
+	    m_brd[x+j][y+j] = player;
+	  }
+	  
+	  return true;
+	}
+      }
+    }
+  }if(y < m_size-1 && x > 0 && player != m_brd[x-1][y+1] && m_brd[x-1][y+1] != "."){
+    //up-right
+    cout << "up-right" << endl;
+    varX = m_size - x;
+    if (y == 0 || x == (m_size - 1)){cout << endl;
+    }else{
+      for(i=2;i<varX;i++){
+	if(player == m_brd[x-i][y+i]){
+	  for(j=1;j<m_size;j++){
+	    
+	    if(m_brd[x-j][y+j] == "."){
+	      return true;
+	    }
+	    m_brd[x-j][y+j] = player;
+	  }
+	  
+	  return true;
+	}
+      }
+    }
+  }else{return false;}
+  
+  return false;
+  
+
+}
+       
 
 Board::~Board(){
 	
-	int i;
-	for (i = 0; i<m_size;i++){
-		delete [] m_brd[i];
-		
-	}
-	delete [] m_brd;
+  int i;
+  for (i = 0; i<m_size;i++){
+    delete [] m_brd[i];
+    
+  }
+  delete [] m_brd;
 }
 int main(){
 	
-	int size;
-	cout << "Please enter the size of the board 4 - 16: " << endl;
-	cin >> size;
-	/*
-	while(size < 4 || size > 16){
-		cout << "Please enter the size of the board 4 - 16: " << endl;
-		cin >> size;
-  }
+  int size;
+  cout << "Please enter the size of the board 4 - 16: " << endl;
+  cin >> size;
+  /*
+    while(size < 4 || size > 16){
+    cout << "Please enter the size of the board 4 - 16: " << endl;
+    cin >> size;
+    }
   */
   Board board(size);
   board.defaultBoard();
-  board.printBoard();
-	return 0;
+  board.makeMovement();
+  return 0;
 }
--- assignment4/reversi.hpp
+++ assignment4/reversi.hpp
@@ -10,19 +10,19 @@ using namespace std;
 
 class Board {
 	
-	public:
-		Board(int size);
-		void printBoard();
-		void makeMovement();
-		bool validMovement(int x, int y, int playerNum);
-		void defaultBoard();
-		~Board();
-	private:
-		int m_size;
-		int m_posX;
-		int m_posY;
-		string **m_brd;
-	
+public:
+  Board(int size);
+  void printBoard();
+  void makeMovement();
+  bool validMovement(int x, int y, int playerNum);
+  void defaultBoard();
+  ~Board();
+private:
+  int m_size;
+  int m_posX;
+  int m_posY;
+  string **m_brd;
+  
 };
 
 
