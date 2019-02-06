dd630009fafd1bc9c70550b9b2c4b62f65bcb816
Naser Alkhateri <nalkhate@ix-dev.cs.uoregon.edu>
2019-02-06 13:30:48 -0800

    validMovement fixed

--- assignment4/reversi.cpp
+++ assignment4/reversi.cpp
@@ -56,6 +56,24 @@ void Board::printBoard(){
     cout << endl;
   }	
 }
+bool Board::isFinished(int playerNum){
+	
+	//checks all cases where available is a valid move exists
+	// if returned false, then there are still possible moves for that player
+	int i, j;
+	for(i = 0; i < m_size; i++){
+		for(j =0; j< m_size; j++){
+			if(validMovement(i,j,playerNum) == true){
+				return false;
+			}
+		}
+		
+	}
+	cout << "No more possible moves for player " << playerNum << endl;
+	return true;
+	
+	
+}
 void Board::makeMovement(){
 
   int playerTurn = 1;
@@ -63,10 +81,11 @@ void Board::makeMovement(){
   bool isOver = false, isValid = false;
   while(!isOver){
     printBoard();
-    cout << "Player " << playerTurn << " turn, input 2 numbers (0-"<< m_size << ") row col: ";
+	//isOver = isFinished(playerTurn);
+    cout << "Player " << playerTurn << " turn, input 2 numbers (0-"<< m_size-1 << ") row col: ";
     cin >> x >> y;
     while(x < 0 || x > m_size - 1 || y < 0 || y > m_size - 1){
-      cout << "Player " << playerTurn << " turn, input 2 numbers (0-"<< m_size << ") row col: ";
+      cout << "Invalid input:Player " << playerTurn << " turn, input 2 numbers (0-"<< m_size-1 << ") row col: ";
       cin >> x >> y;
     }
     isValid = validMovement(x, y, playerTurn);
@@ -96,7 +115,7 @@ bool Board::validMovement(int x, int y, int playerNum){
 	cout << "entered\n";
 	for(i=2;i<=y;i++){
       if(player == m_brd[x][y-i]){
-		for(j=1;j<m_size;j++){
+		for(j=1;j<=y;j++){
 	  
 			if(m_brd[x][y-j] == "."){
 				return true;
@@ -113,10 +132,10 @@ bool Board::validMovement(int x, int y, int playerNum){
     //right
     cout << "right" << endl;
     varY = m_size - y;
-    if (x == (m_size - 1)){}
+
     for(i=2;i<varY;i++){
       if(player == m_brd[x][y+i]){
-	for(j=1;j<m_size;j++){
+	for(j=1;j<varY;j++){
 	  
 	  if(m_brd[x][y+j] == "."){
 	    return true;
@@ -131,11 +150,10 @@ bool Board::validMovement(int x, int y, int playerNum){
     //down
     cout << "down" << endl;
     varX = m_size - x;
-    if (y == (m_size - 1)){cout << endl;
-    }else{
+ 
       for(i=2;i<varX;i++){
 	if(player == m_brd[x+i][y]){
-	  for(j=1;j<m_size;j++){
+	  for(j=1;j<varX;j++){
 	    
 	    if(m_brd[x+j][y] == "."){
 	      return true;
@@ -145,37 +163,35 @@ bool Board::validMovement(int x, int y, int playerNum){
 	  return true;
 	}
       }
-    }
+    
   }
   if(x > 0 && player != m_brd[x-1][y] && m_brd[x-1][y] != "."){
     //up
 	cout << "up" << endl;
-    if (y == 0){cout << endl;
-    }else{
+
       varX = m_size - x;
-      for(i=2;i<varX;i++){
-	if(player == m_brd[x-i][y]){
-	  for(j=1;j<m_size;j++){
+      for(i=2;i<=x;i++){
+		if(player == m_brd[x-i][y]){
+			for(j=1;j<=x;j++){
 	    
-	    if(m_brd[x-j][y] == "."){
-	      return true;
+				if(m_brd[x-j][y] == "."){
+					return true;
 	    }
-	    m_brd[x-j][y] = player;
+				m_brd[x-j][y] = player;
 	  }
 	  return true;
 	}
       }
-    }
+    
   }
   if(y > 0 && x < m_size-1 && player != m_brd[x+1][y-1] && m_brd[x+1][y-1] != "."){
     //down-left
     cout << "down-left" << endl;
-    if (y == (m_size - 1) || x == 0 ){cout << endl;
-    }else{
+    
       varX = m_size - x;
-      for(i=2;i<varX;i++){
+      for(i=2;i<varX || i<=y;i++){
 	if(player == m_brd[x+i][y-i]){
-	  for(j=1;j<m_size;j++){
+	  for(j=1;j<varX || j<=y;j++){
 	    
 	    if(m_brd[x+j][y-j] == "."){
 	      return true;
@@ -186,16 +202,15 @@ bool Board::validMovement(int x, int y, int playerNum){
 	  return true;
 	}
       }
-    }
+    
   }if(y > 0 && x > 0 && player != m_brd[x-1][y-1] && m_brd[x-1][y-1] != "."){
     //up-left
     cout << "up-left" << endl;
     varX = m_size - x;
-    if (x == 0 || y == 0){cout << endl;
-    }else{
-      for(i=2;i<varX;i++){
+
+      for(i=2;i<=x || i<=y;i++){
 	if(player == m_brd[x-i][y-i]){
-	  for(j=1;j<m_size;j++){
+	  for(j=1;j<=x || j<=y;j++){
 	    
 	    if(m_brd[x-j][y-j] == "."){
 	      return true;
@@ -206,15 +221,15 @@ bool Board::validMovement(int x, int y, int playerNum){
 	  return true;
 	}
       }
-    }
+ 
   }if(y < m_size-1 && x < m_size-1 && player != m_brd[x+1][y+1] && m_brd[x+1][y+1] != "."){
+	//down-right
 	cout << "down-right" << endl;
     varX = m_size - x;
-    if (x == (m_size - 1) || y == (m_size - 1)){cout << endl;
-    }else{
-      for(i=2;i<varX;i++){
+    varY = m_size - y;
+      for(i=2;i<varX || i<varY;i++){
 	if(player == m_brd[x+i][y+i]){
-	  for(j=1;j<m_size;j++){
+	  for(j=1;j<varX || j<varY;j++){
 				  
 	    if(m_brd[x+j][y+j] == "."){
 	      return true;
@@ -225,16 +240,15 @@ bool Board::validMovement(int x, int y, int playerNum){
 	  return true;
 	}
       }
-    }
+    
   }if(y < m_size-1 && x > 0 && player != m_brd[x-1][y+1] && m_brd[x-1][y+1] != "."){
     //up-right
     cout << "up-right" << endl;
     varX = m_size - x;
-    if (y == 0 || x == (m_size - 1)){cout << endl;
-    }else{
-      for(i=2;i<varX;i++){
+	varY = m_size - y;
+      for(i=2;i<=x || i<varY;i++){
 	if(player == m_brd[x-i][y+i]){
-	  for(j=1;j<m_size;j++){
+	  for(j=1;j<=x || j<varY;j++){
 	    
 	    if(m_brd[x-j][y+j] == "."){
 	      return true;
@@ -245,7 +259,7 @@ bool Board::validMovement(int x, int y, int playerNum){
 	  return true;
 	}
       }
-    }
+    
   }else{return false;}
   
   return false;
