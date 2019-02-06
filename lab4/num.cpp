ed3fcd25633b4ba9f58570cab7a432eca478d348
Naser Alkhateri <nalkhate@ix-dev.cs.uoregon.edu>
2019-02-05 19:34:15 -0800

    lab4 added

---
+++ lab4/num.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+  int **num;
+  int size = 5, i, j;
+  
+  // Allocate memory for 2-D int array
+  num = new int * [size];
+  for (i = 0; i < size; i++){
+    num[i] = new int [size];
+    for (j = 0; j < size; j++)
+      num[i][j] = i * size + j;
+}
+  // Print values
+  for (i = 0; i < size; i++) {
+    for (j = 0; j < size; j++) {
+      printf("%d ", num[i][j]);
+    }
+    printf("\n");
+	}
+  
+  // Deallocate memory
+  for (i = 0; i < size; i++)
+    delete [] num[i];
+  delete [] num;
+    
+}
